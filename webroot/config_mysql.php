f48498776ff6361dceed9dc57ce8dc03d6510ca1
Mikael Roos <mikael.t.h.roos@gmail.com>
2014-04-17 11:32:55 +0200

    Adding debug mode to hide or display conneciton details when connect() fails

--- webroot/config_mysql.php
+++ webroot/config_mysql.php
@@ -7,4 +7,5 @@ return [
     'driver_options'  => [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
     'table_prefix'    => "test_",
     'verbose' => true,
+    //'debug_connect' => 'true',
 ];
