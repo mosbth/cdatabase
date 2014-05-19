6b71c2aa75169fe875c3267b82115ada0e88542c
Mikael Roos <mikael.t.h.roos@gmail.com>
2014-05-19 09:38:17 +0200

    Adding error reporting as default

--- webroot/testMySQL.php
+++ webroot/testMySQL.php
@@ -1,5 +1,16 @@
 <?php
 
+//
+// Set the error reporting.
+//
+error_reporting(-1);              // Report all type of errors
+ini_set('display_errors', 1);     // Display all errors
+ini_set('output_buffering', 0);   // Do not buffer outputs, write directly
+
+
+//
+// Get required files
+//
 require "../src/Database/TSQLQueryBuilderBasic.php";
 require "../src/Database/CDatabaseBasic.php";
 
