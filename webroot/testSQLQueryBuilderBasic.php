7eac5bd4b32df1342c2803ec6310a9400ab6d80e
jonatan karlsson <me@jonatankarlsson.se>
2015-03-10 20:00:55 +0100

    Added right, left - join() instead of use same function for every join call

--- webroot/testSQLQueryBuilderBasic.php
+++ webroot/testSQLQueryBuilderBasic.php
@@ -154,6 +154,19 @@ $db->select("t1.*, t2.*, t3.*")
 echo "<pre>" . $db->getSQL() . "</pre>";
 
 
+$db->select("t1.*, t2.id AS id2, t3.id AS id3")
+    ->from('test AS t1')
+    ->rightJoin('test AS t2', 't1.id = t2.id')
+    ->rightJoin('test AS t3', 't1.id = t3.id');
+
+echo "<pre>" . $db->getSQL() . "</pre>";
+
+$db->select("t1.*, t2.id AS id2, t3.id AS id3")
+    ->from('test AS t1')
+    ->leftJoin('test AS t2', 't1.id = t2.id')
+    ->leftJoin('test AS t3', 't1.id = t3.id');
+
+echo "<pre>" . $db->getSQL() . "</pre>";
 
 //
 // Select, limit and offset
